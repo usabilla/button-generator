6cce60f0782b86e745a4b8f506be027f081495bc
Katia Agnamazian <k.agnamazian@gmail.com>
2017-01-13 11:55:16 +0100

    first test running

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,14 +1,41 @@
-const App = require('../lib/app');
+const fs = require('fs');
+const path = require('path');
+const ButtonGenerator = require('../lib/app');
 
-describe('App', function() {
+const SOURCE_SVG = path.resolve('./lib/resources/button.svg');
+const BUILD_DIR = path.resolve('./test');
+
+describe('ButtonGenerator', function() {
   beforeEach(function() {
-    this.app = new App();
+    this.buttonGenerator = new ButtonGenerator();
+    spyOn(this.buttonGenerator, 'svg2png').and.callThrough();
   });
 
-  describe('#foo', function() {
-    it('returns foo', function() {
-      expect(this.app.foo()).toBe('foo');
+  describe('#generatePNG', function() {
+
+    it('accepts a SVG file and generates a PNG into the given directory', function(done) {
+      this.buttonGenerator.generatePNG(SOURCE_SVG, BUILD_DIR)
+        .then(() => {
+          const files = fs.readdirSync(BUILD_DIR);
+          expect(path.parse(files[2]).ext).toBe('.png'); //TODO index need to be dynamic
+          done();
+        })
+      .catch(done.fail);
     });
+
+    it('uses svg2png to convert files', function(done) {
+      this.buttonGenerator.generatePNG(SOURCE_SVG, BUILD_DIR)
+        .then(() => {
+          expect(this.buttonGenerator.svg2png).toHaveBeenCalled();
+          done();
+        })
+      .catch(done.fail);
+    });
+
+    //it('uses SVG file name for PNG filename');
+
+
+
   });
 
 });
