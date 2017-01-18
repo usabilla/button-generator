d6537d5d146b967d2a5303548ef688df019a2e51
Katia Agnamazian <k.agnamazian@gmail.com>
2017-01-18 10:13:24 +0100

    Modification unit test with return value

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,27 +1,29 @@
 const ButtonGenerator = require('../lib/app');
 const fs = require('fs-promise');
 const faker = require('faker');
+const path = require('path');
 
 describe('ButtonGenerator', function() {
 
   beforeEach(function() {
     this.buttonGenerator = new ButtonGenerator();
     this.directoryPath = './dest';
+    this.fileName = faker.system.fileName();
     this.source = faker.image.imageUrl();
-
-    spyOn(this.buttonGenerator, 'svg2png').and.returnValue(Promise.resolve());
-    spyOn(fs, 'readFile').and.returnValue(Promise.resolve());
+    this.buffer = {};
+    spyOn(this.buttonGenerator, 'svg2png').and.returnValue(Promise.resolve(this.buffer));
+    spyOn(fs, 'readFile').and.returnValue(Promise.resolve(this.buffer));
     spyOn(fs, 'writeFile').and.returnValue(Promise.resolve());
   });
 
   describe('#generatePNG', function() {
 
     it('reads file, converts to png, and writes it', function(done) {
-      this.buttonGenerator.generatePNG(this.source, this.directoryPath, faker.system.fileName())
+      this.buttonGenerator.generatePNG(this.source, this.directoryPath, this.fileName)
         .then(() => {
           expect(fs.readFile).toHaveBeenCalledWith(this.source);
-          expect(this.buttonGenerator.svg2png).toHaveBeenCalled();
-          expect(fs.writeFile).toHaveBeenCalled();
+          expect(this.buttonGenerator.svg2png).toHaveBeenCalledWith(this.buffer);
+          expect(fs.writeFile).toHaveBeenCalledWith(path.join(this.directoryPath, `${this.fileName}.png`),this.buffer);
           done();
         })
         .catch(done.fail);
