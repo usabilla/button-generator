89040765258edcc0a0615843a640ae2625038aae
Katia Agnamazian <k.agnamazian@gmail.com>
2017-01-23 14:40:52 +0100

    Add fontName to options list + ignore output folder with generated files

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const ButtonGenerator = require('./lib/button-generator');
 //Just for test
 const options = {
   fontUrl: '//fonts.googleapis.com/css?family=Open+Sans',
+  fontName: 'Open Sans',
   backgroundColor: '#000000',
   textColor: '#fff',
   hasSoftCorner: false,
