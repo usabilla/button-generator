241a1bb9441349655f078a1de478298d6dc7a67f
Katia Agnamazian <k.agnamazian@gmail.com>
2017-01-24 14:21:15 +0100

    Use sharp module for converting svg to png

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-const App = require('./lib/app');
+const ButtonGenerator = require('./lib/app');
 
-/*Util for running module with command-line*/
-if (process.argv.length != 9) {
-  console.log('not enough arguments');
-} else {
-  let options = {
-    fontUrl: process.argv[2],
-    backgroundColor: process.argv[3],
-    textColor: process.argv[4],
-    content: process.argv[5],
-    hasSoftCorner: process.argv[6],
-    size: {
-      width: process.argv[7],
-      height: process.argv[8]
-    }
-  }
-  console.log(options); //TODO don't trust input - check data type
-  (new App()).foo();
-}
+
+(new ButtonGenerator()).generatePNG('/Users/Katia/.usabilla/src/button-generator/buttons/usabilla_feedback_button_desktop_right.svg', '/Users/Katia/.usabilla/src/button-generator/buttons/', 'result');
+
