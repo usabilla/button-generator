15666ecb8608ebe94d6f07c417019a4586aafd41
pietvanzoen <pietvanzoen@gmail.com>
2017-03-13 13:48:33 +0100

    fix(font): custom font css applies correctly
    Custom font css was not being applied to the button unless the font
    already existed in your local font directory. Fonts from the CSS file
    were not loaded.
    
    Fix is to use svg2png which renders the svg buffer in PhantomJS and
    pulls an image from there.
    
    Also removed some extrenuous methods that cuased issues when applying
    fix and were not being used anywhere.
    
    Closes https://usabilla.atlassian.net/browse/FOO-63

--- test/button-generator.spec.js
+++ test/button-generator.spec.js
@@ -1,7 +1,6 @@
 const ButtonGenerator = require('../lib/button-generator');
 const ButtonSVG = require('../lib/button-svg');
 const fs = require('fs-promise');
-const faker = require('faker');
 
 describe('ButtonGenerator', function() {
 
@@ -21,18 +20,11 @@ describe('ButtonGenerator', function() {
     this.buttonG = new ButtonGenerator(this.options);
     spyOn(fs, 'mkdirs').and.returnValue(Promise.resolve());
     spyOn(fs, 'writeFile').and.returnValue(Promise.resolve());
-    const toFile = jasmine.createSpy('toFile');
-    const toBuffer = jasmine.createSpy('toBuffer');
-    this.sharpObj = {
-      toFile,
-      toBuffer,
-      rotate: function() {
-        return this;
-      }
-    };
-    ButtonGenerator.sharp = () => this.sharpObj;
-    spyOn(ButtonGenerator, 'sharp').and.callThrough();
-    spyOn(this.sharpObj, 'rotate').and.callThrough();
+    this.sharpObj = jasmine.createSpyObj('sharpObj', ['toBuffer', 'rotate']);
+    this.sharpObj.rotate.and.returnValue(this.sharpObj);
+    this.sharpObj.toBuffer.and.returnValue(Promise.resolve());
+    spyOn(ButtonGenerator, 'sharp').and.returnValue(this.sharpObj);
+    spyOn(ButtonGenerator, 'svg2png').and.returnValue(Promise.resolve());
   });
 
   describe('#generate', function() {
@@ -41,48 +33,27 @@ describe('ButtonGenerator', function() {
       spyOn(this.buttonSVG, 'generate').and.returnValue(Promise.resolve());
       spyOn(ButtonSVG, 'stringify').and.returnValue('');
     });
+
     it('calls stringify function', function() {
       this.buttonG.generate();
       expect(ButtonSVG.stringify).toHaveBeenCalled();
     });
 
-    it('calls sharp module methods', function() {
-      this.buttonG.generate();
-      expect(ButtonGenerator.sharp).toHaveBeenCalled();
-      expect(this.sharpObj.rotate).toHaveBeenCalledWith(this.buttonG.getOrientationAngle());
+    it('calls sharp module methods', function(done) {
+      expect(this.buttonG.generate()).toResolve(done, () => {
+        expect(ButtonGenerator.sharp).toHaveBeenCalled();
+        expect(this.sharpObj.rotate).toHaveBeenCalledWith(this.buttonG.getOrientationAngle());
+        expect(this.sharpObj.toBuffer).toHaveBeenCalled();
+      });
+    });
+
+    it('calls svg2png', function(done) {
+      expect(this.buttonG.generate()).toResolve(done, () => {
+        expect(ButtonGenerator.svg2png).toHaveBeenCalled();
+      });
     });
   });
 
-  describe('#getBuffer', function() {
-    it('calls toBuffer', function() {
-      this.buttonG.getBuffer();
-      expect(this.sharpObj.toBuffer).toHaveBeenCalled();
-    });
-  });
-
-  describe('#getPng', function() {
-    it('calls toFile', function() {
-      this.buttonG.getPng();
-      expect(this.sharpObj.toFile).toHaveBeenCalledWith(ButtonGenerator.getOutputPath('png'));
-    });
-
-    it('returns filepath', function() {
-      expect(this.buttonG.getPng()).toEqual(ButtonGenerator.getOutputPath('png'));
-    });
-  });
-
-  describe('::getOutputPath', function() {
-    it('returns a path which is a string', function() {
-      const path = ButtonGenerator.getOutputPath(faker.system.fileExt());
-      expect(path).toMatch('^(.+)\/([^/]+)$');
-    });
-
-    it('returns a path that includes the correct extension', function() {
-      const path = ButtonGenerator.getOutputPath('svg');
-      const pathsGroup = path.split('.');
-      expect(pathsGroup[pathsGroup.length - 1]).toBe('svg');
-    })
-  });
 
   describe('::getOrientationAngle', function() {
     it('returns 0 if orientation not found', function() {
